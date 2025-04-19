6c0960f0d65ad013474e63639811f452ad737526
Eugênio Barros <eugenio.barros.jr@gmail.com>
2025-04-19 15:43:21 -0300

    Time: 2 ms (53.45%) | Memory: 19.1 MB (30.37%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,21 +1,13 @@
-#     for (let currentIndex = 1; currentIndex < nums.length; currentIndex++) {
-#         if (nums[uniqueIndex] < nums[currentIndex]) {
-#             uniqueIndex++;
-#             nums[uniqueIndex] = nums[currentIndex];
-#         }
-#     }
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) == 0:
+        if not nums:
             return 0
-        elif len(nums) == 1:
-            return 1
-        else:
-            uniqueIndex = 0
+        
+        unique_index = 0
 
-            for i in range(1, len(nums)):
-                if nums[uniqueIndex] < nums[i]:
-                    uniqueIndex += 1
-                    nums[uniqueIndex] = nums[i]
-
-            return uniqueIndex + 1        
+        for i in range(1, len(nums)):
+            if nums[i] != nums[unique_index]:
+                unique_index += 1
+                nums[unique_index] = nums[i]
+        
+        return unique_index + 1     
